81459b2f0bccc05db420d11f043210a7a2d36e7d
Nataly Kulychyk <nkul@ciklum.com>
2018-03-15 18:45:22 +0200

    add command url"

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ import logging
 from aiohttp import web
 from aiohttp.web_response import json_response
 
+import pprint
+
 from .lib import Dispatcher
 
 log = logging.getLogger('application')
@@ -21,6 +23,14 @@ async def github_hook(request):
 async def health(_):
     return json_response({'ok': True})
 
+
+async def auth(request):
+    # request_json = await request.json()
+    print(await request.text())
+    return json_response({'ok': True})
+
+
 app = web.Application()
 app.router.add_post('/poke', github_hook)
+app.router.add_post('/auth', auth)
 app.router.add_get('/_healthz', health)
